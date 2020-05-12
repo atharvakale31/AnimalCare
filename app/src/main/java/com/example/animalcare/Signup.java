cc92d0b64edbc40cc088472e6085615b2ba2080a
Swaraj_Dolhare <swarajbdolhare@gmail.com>
2020-05-12 23:28:35 +0530

    loginpage

--- app/src/main/java/com/example/animalcare/Signup.java
+++ app/src/main/java/com/example/animalcare/Signup.java
@@ -2,18 +2,16 @@ package com.example.animalcare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
-import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
-    FirebaseAuth fb;
+public class Signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
+                
 
 
 
-    }
+        }
 }
