c76935c0b35ec33238716be59d072cb198e4ad16
Swaraj_Dolhare <swarajbdolhare@gmail.com>
2020-05-15 23:30:10 +0530

    signup page linked with firebase!!!

--- app/src/main/java/com/example/animalcare/MainActivity.java
+++ app/src/main/java/com/example/animalcare/MainActivity.java
@@ -1,19 +1,29 @@
 package com.example.animalcare;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.Button;
+
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth fb;
+    Button signUp= findViewById(R.id.Signupbtn1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,Signup.class));
+            }
+        });
 
     }
 }
