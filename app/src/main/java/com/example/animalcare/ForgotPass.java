97dd77fd6896b92a3bcd5dbb4b12d632b579aaa1
Atharva Kale <atharvakale31@gmail.com>
2020-05-20 14:17:08 +0530

    Signup page and login page UI change

---
+++ app/src/main/java/com/example/animalcare/ForgotPass.java
@@ -0,0 +1,62 @@
+package com.example.animalcare;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ForgotPass extends AppCompatActivity {
+
+    FirebaseAuth fb;
+    TextView email;
+    Button reset;
+    public void resetpass (View view)
+    {
+        String stremail= email.getText().toString();
+
+        if(stremail.isEmpty())
+        {
+            email.setError("Please enter your email id");
+            email.requestFocus();
+        }
+        else {
+            fb.sendPasswordResetEmail(stremail)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(ForgotPass.this, "Email Sent", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+        }
+
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forgotpass);
+        fb=FirebaseAuth.getInstance();
+
+        email=findViewById(R.id.frgtemail);
+        reset=findViewById(R.id.resetbutton);
+
+
+
+
+    }
+}
