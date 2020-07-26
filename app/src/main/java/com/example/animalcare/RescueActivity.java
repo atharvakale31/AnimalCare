8258610d6cea3167c1c424cba84cb1cedcd0fb3b
Atharva Kale <atharvakale31@gmail.com>
2020-07-26 22:01:26 +0530

    Added Map navigation

--- app/src/main/java/com/example/animalcare/RescueActivity.java
+++ app/src/main/java/com/example/animalcare/RescueActivity.java
@@ -2,8 +2,20 @@ package com.example.animalcare;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,26 +23,72 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.messaging.FirebaseMessaging;
 
-public class RescueActivity extends AppCompatActivity {
+public class RescueActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    Button rescueLogOut;
+    Button rescueLogOut,nav;
     FirebaseAuth firebaseAuth;
     FirebaseUser firebaseUser;
-    String a,b,c,d,e,f;
+    String a,b,c,d="0",e="0",f;
     TextView name,phone,animal,latLng,loc;
+    GoogleMap mMap;
+    LocationManager locationManager;
+    LocationListener locationListener;
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rescue);
+        nav=findViewById(R.id.navigate);
+        locationManager=(LocationManager)getSystemService(LOCATION_SERVICE);
+        locationListener=new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                LatLng yourlocation = new LatLng(location.getLatitude(),location.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(yourlocation).title("Your Position"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(yourlocation));
+                Log.i("Location",location.toString());
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
 
         subscribeToTopic();
+
 
         rescueLogOut = findViewById(R.id.BtnRescueLogout);
         name = findViewById(R.id.RuserName);
@@ -53,9 +111,12 @@ public class RescueActivity extends AppCompatActivity {
             }
         });
 
+
+
+
         Bundle bundle = getIntent().getExtras();
         if(bundle!=null){
-            Log.i("OKISHERE","HOLA");
+            //Log.i("OKISHERE","HOLA");
             if(bundle.getString("FromWhere","NO").equals("NOTIF")) {
                 a = bundle.getString("USERNAME", "UserName");
                 b = bundle.getString("PHONENO", "Phone No");
@@ -63,13 +124,35 @@ public class RescueActivity extends AppCompatActivity {
                 d = bundle.getString("LAT", "Lati");
                 e = bundle.getString("LNG", "Longi");
                 f = bundle.getString("Location", "Location?");
-                Log.i("GETFINALHERE", a + " " + b + " " + c + " " + d + " " + e + " " + f);
+                //Log.i("GETFINALHERE", a + " " + b + " " + c + " " + d + " " + e + " " + f);
                 name.setText(a);
                 phone.setText(b);
                 loc.setText(f);
                 animal.setText(c);
                 latLng.setText(d + "," +e);
+                SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+
+                mapFragment.getMapAsync(this);
                 Toast.makeText(this, "Rescue Details Loaded", Toast.LENGTH_SHORT).show();
+                nav.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Uri gmmIntentUri = Uri.parse("google.navigation:q="+d+","+e+"");
+                        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                        mapIntent.setPackage("com.google.android.apps.maps");
+
+                        startActivity(mapIntent);
+                        //"google.navigation:q=Taronga+Zoo,+Sydney+Australia"
+
+                    }
+                });
+
+
+
+
+
+
             }
         }
         Log.i("GETISHERE","OKOK");
@@ -90,4 +173,35 @@ public class RescueActivity extends AppCompatActivity {
                 });
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }
+        else
+        {
+            mMap = googleMap;
+
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            Location lastKnownLocation=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            LatLng yourlocation = new LatLng(lastKnownLocation.getLatitude(),lastKnownLocation.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(yourlocation).title("Your Position").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(yourlocation));
+            double lat =Double.parseDouble(d);
+            double lon=Double.parseDouble(e);
+            Toast.makeText(this, "IN MAP", Toast.LENGTH_SHORT).show();
+            // Add a marker in Sydney and move the camera
+            LatLng rescue=new LatLng(lat,lon);
+           mMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(lat,lon))
+                    .title("Marker"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(rescue,13));
+
+        }
+        
+
+
+    }
 }
