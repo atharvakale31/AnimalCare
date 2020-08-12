3672d04cdb9dce7ae54966e575cf8faecf5dbf2e
Supriy Jinturkar <supriyjinturkar@gmail.com>
2020-08-13 00:35:53 +0530

    Added Phone Auth Verification using OTP

--- app/src/main/java/com/example/animalcare/AnimalRescueLoginActivity.java
+++ app/src/main/java/com/example/animalcare/AnimalRescueLoginActivity.java
@@ -3,16 +3,22 @@ package com.example.animalcare;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -20,24 +26,24 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class AnimalRescueLoginActivity extends AppCompatActivity {
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        Intent intent = new Intent(AnimalRescueLoginActivity.this,MainActivity.class);
-        finish();
-        startActivity(intent);
-    }
-
-    EditText usernameET , passwordET;
+//    @Override
+//    public void onBackPressed() {
+//        super.onBackPressed();
+//        Intent intent = new Intent(AnimalRescueLoginActivity.this,MainActivity.class);
+//        finish();
+//        startActivity(intent);
+//    }
+    private TextView textViewRescueSignUp;
+    EditText volunteerPhoneNo , passwordET;
     Button loginbtn;
-    FirebaseDatabase firebaseDatabase;
+    FirebaseFirestore firebaseFirestore;
     FirebaseAuth firebaseAuth;
-    DatabaseReference databaseReference;
-    String tempPwd = "";
-    boolean pwdMatched= false;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,54 +51,72 @@ public class AnimalRescueLoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_animal_rescue_login);
 
         firebaseAuth = FirebaseAuth.getInstance();
-        firebaseDatabase = FirebaseDatabase.getInstance();
-
-        usernameET = findViewById(R.id.EditTextRescueUsername);
-        passwordET = findViewById(R.id.EditTextRescuePassword);
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        progressDialog = new ProgressDialog(AnimalRescueLoginActivity.this);
+        progressDialog.setMessage("Checking your phone no...");
+        progressDialog.setTitle("Account LogIn");
+        progressDialog.setCancelable(false);
+        volunteerPhoneNo = findViewById(R.id.EditTextRescueUsername);
+        //passwordET = findViewById(R.id.EditTextRescuePassword);
         loginbtn = findViewById(R.id.BtnRescueLogin);
+        textViewRescueSignUp = findViewById(R.id.textViewRescueSignUp);
+
+        textViewRescueSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(AnimalRescueLoginActivity.this,RescueSignUpActivity.class));
+            }
+        });
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String userName = usernameET.getText().toString();
-                String password = passwordET.getText().toString();
+                String phoneNo = volunteerPhoneNo.getText().toString();
+                //String password = passwordET.getText().toString();
 
-                if(TextUtils.isEmpty(userName)){
-                    usernameET.setError("Please enter the email!");
+                if(TextUtils.isEmpty(phoneNo)){
+                    volunteerPhoneNo.setError("Please enter your Phone No.");
                     return;
                 }
-                if(TextUtils.isEmpty(password)){
-                    passwordET.setError("Please enter the password!");
-                    return;
-                }
-                if(!TextUtils.isEmpty(userName) && !TextUtils.isEmpty(password)){
+//                if(TextUtils.isEmpty(password)){
+//                    passwordET.setError("Please enter the password!");
+//                    return;
+//                }
 
-                    firebaseAuth.signInWithEmailAndPassword(userName,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                if(!checkPhoneNo(phoneNo)){
+                    Snackbar snackbar = Snackbar.make(volunteerPhoneNo, "Please enter 10 digit Phone No.", Snackbar.LENGTH_LONG);
+                    snackbar.setAction("OK", new View.OnClickListener() {
                         @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(task.isSuccessful()){
-                                Toast.makeText(AnimalRescueLoginActivity.this, "Login Successful :)", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(AnimalRescueLoginActivity.this,VolunteerHomeActivity.class);
-                                finish();
-                                startActivity(intent);
-                            }else{
-                                Toast.makeText(AnimalRescueLoginActivity.this, "Login Failed :)", Toast.LENGTH_SHORT).show();
-                                //Intent intent = new Intent(AnimalRescueLoginActivity.this,MainActivity.class);
-                                //finish();
-                                //startActivity(intent);
-                            }
+                        public void onClick(View v) {
+
                         }
                     });
+                    snackbar.show();
+                    return;
+                }
 
-//                    databaseReference = firebaseDatabase.getReference("RescueData");
-//                    databaseReference.child(userName).addListenerForSingleValueEvent(new ValueEventListener() {
+                if(!TextUtils.isEmpty(phoneNo)){//&& !TextUtils.isEmpty(password)){
+
+                    progressDialog.show();
+                    checkVolunteerPhoneNo(phoneNo);
+                    //startPhoneVerification(phoneNo);
+//                    firebaseAuth.signInWithEmailAndPassword(userName,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
 //                        @Override
-//                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                            if(dataSnapshot.getValue()!=null){
-//                               tempPwd = dataSnapshot.getValue().toString();
+//                        public void onComplete(@NonNull Task<AuthResult> task) {
+//                            if(task.isSuccessful()){
+//                                Toast.makeText(AnimalRescueLoginActivity.this, "Login Successful :)", Toast.LENGTH_SHORT).show();
+//                                Intent intent = new Intent(AnimalRescueLoginActivity.this,VolunteerHomeActivity.class);
+//                                finish();
+//                                startActivity(intent);
+//                            }else{
+//                                Toast.makeText(AnimalRescueLoginActivity.this, "Login Failed :)", Toast.LENGTH_SHORT).show();
+//                                //Intent intent = new Intent(AnimalRescueLoginActivity.this,MainActivity.class);
+//                                //finish();
+//                                //startActivity(intent);
 //                            }
-//                        }
-//
+ //                       }
+ //                   });
 
 
                 }
@@ -100,5 +124,112 @@ public class AnimalRescueLoginActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void checkVolunteerPhoneNo(final String phoneNo){
+        firebaseAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            checkOnDatabase(phoneNo);
+                            //firebaseAuth.signOut();
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("SigInANO", "signInAnonymously:success");
+                            //FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            if(progressDialog.isShowing())
+                                progressDialog.cancel();
+                                // If sign in fails, display a message to the user.
+                            Log.w("SigInANO", "signInAnonymously:failure", task.getException());
+                            Toast.makeText(AnimalRescueLoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                    }
+                });
+    }
+
+    public void checkOnDatabase(final String phoneNo){
+        Log.d("USERANO","TRUE");
+        firebaseFirestore.collection("VolunteerProfileData")
+                .whereEqualTo("volunteerPhoneNo",phoneNo)
+                .get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(final QuerySnapshot queryDocumentSnapshots) {
+                //firebaseAuth.signOut();
+                if(progressDialog.isShowing())
+                    progressDialog.cancel();
+
+                if(firebaseAuth.getCurrentUser()!=null) {
+                    firebaseAuth.getCurrentUser().delete()
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Log.d("DELETEACC", "User account deleted.");
+                                        if(queryDocumentSnapshots!=null && !queryDocumentSnapshots.isEmpty()){
+
+                                            startPhoneVerification(phoneNo);
+                                            //Toast.makeText(AnimalRescueLoginActivity.this, "OK good", Toast.LENGTH_SHORT).show();
+                                        }
+                                        else{
+                                            Snackbar snackbar = Snackbar.make(volunteerPhoneNo, "Phone No. not registered, Please create a new account !", Snackbar.LENGTH_LONG);
+                                            snackbar.setAction("OK", new View.OnClickListener() {
+                                                @Override
+                                                public void onClick(View v) {
+
+                                                }
+                                            });
+                                            snackbar.show();
+                                        }
+                                    }
+                                }
+                            });
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                //firebaseAuth.signOut();
+                if(progressDialog.isShowing())
+                    progressDialog.cancel();
+
+                if(firebaseAuth.getCurrentUser()!=null) {
+                    firebaseAuth.getCurrentUser().delete()
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Log.d("DELETEUUUSER", "User account deleted.");
+                                    }
+                                }
+                            });
+                    Log.d("USERANOERROR", e.toString());
+                    Snackbar snackbar = Snackbar.make(volunteerPhoneNo, "Some error occurred, Please try ", Snackbar.LENGTH_LONG);
+                    snackbar.setAction("OK", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                        }
+                    });
+                    snackbar.show();
+                }
+            }
+        });
+    }
+
+    public void startPhoneVerification(String volunteerPhone){
+        Intent intent = new Intent(AnimalRescueLoginActivity.this,PhoneVerificationActivity.class);
+        intent.putExtra("phoneNo",volunteerPhone);
+        intent.putExtra("isSignUp",false);
+        startActivity(intent);
+    }
+
+    public boolean checkPhoneNo(String phoneNo){
+
+        return phoneNo.matches("[0-9]{10}");
     }
 }
